3ab92487bc211ede520eb11409729cfa51e4dbfe
Michael Hacker <hack1973@swbell.net>
2020-10-31 21:59:50 -0500

    Completed all exercises

---
+++ src/exercises/Alice.java
@@ -0,0 +1,37 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Alice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String sentence = "Alice was beginning to get very tired of sitting by her sister on the bank," +
+                " and of having nothing to do: once or twice she had peeped into the book her sister was reading," +
+                " but it had no pictures or conversations in it, 'and what is the use of a book,'" +
+                " thought Alice 'without pictures or conversation?'";
+
+        System.out.println("What 'word' would you like to search for in the first sentence of the 'Alice In Wonderland' book:");
+        String searchTerm = input.nextLine();
+
+        boolean answer = sentence.toLowerCase().contains(searchTerm.toLowerCase());
+        System.out.println("\nThe first sentence of the book:\n" + sentence);
+        System.out.println("\nWas the word '" + searchTerm + "' found in the first sentence of the 'Alice In Wonderland' book?(true or false): " + answer);
+
+        if (answer) {
+            String newSentence0;
+            String newSentence1;
+            int searchTermIndex = sentence.toLowerCase().indexOf(searchTerm.toLowerCase());
+            System.out.println("The index of the word '" + searchTerm + "' in the sentence is: " + searchTermIndex);
+            System.out.println("The length of the word '" + searchTerm + "' is " + searchTerm.length() + " characters long.");
+            newSentence1 = sentence.substring(searchTermIndex + searchTerm.length());
+
+            if (searchTermIndex == 0) {
+                System.out.println("The new sentence with the word '" + searchTerm + "' removed is:\n" + newSentence1);
+            } else {
+                newSentence0 = sentence.substring(0,searchTermIndex - 1);
+                System.out.println("The new sentence with the word '" + searchTerm + "' removed is:\n" + newSentence0 + newSentence1);
+            }
+        }
+    }
+
+}
---
+++ src/exercises/Mpg.java
@@ -0,0 +1,18 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Mpg {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many miles have you driven:");
+        Double miles = input.nextDouble();
+
+        System.out.println("How many gallons of gas have you consumed:");
+        Double gallons = input.nextDouble();
+
+        double mpg = miles / gallons;
+        System.out.println("You have got " + mpg + " miles to the gallon.");
+    }
+}
