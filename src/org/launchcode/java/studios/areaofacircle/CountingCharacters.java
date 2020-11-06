011a72348a64dca04ee17bed961674d3d5686b88
Michael Hacker <hack1973@swbell.net>
2020-11-05 21:29:23 -0600

    Completed CH3 Studio - bonus missions

---
+++ src/org/launchcode/java/studios/areaofacircle/CountingCharacters.java
@@ -0,0 +1,30 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountingCharacters {
+    public static void main(String[] args) {
+        String paragraph = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        char[] charactersInParagraph = paragraph.toCharArray();
+        HashMap<Character,Integer> characterCount = new HashMap<>();
+
+        for (char character : charactersInParagraph) {
+
+            if (characterCount.containsKey(character)) {
+                Integer count = characterCount.get(character);
+                characterCount.put(character,count+1);
+            } else {
+                characterCount.put(character,1);
+            }
+        }
+
+        for (Map.Entry<Character,Integer> character : characterCount.entrySet()) {
+            System.out.println(character.getKey() + ": " + character.getValue());
+        }
+
+
+
+
+    }
+}
